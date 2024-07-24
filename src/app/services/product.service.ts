9950015fb9243f34ae65f6be5b8a70fcfbd39163
Franz Bendezu <franzant20@gmail.com>
2024-07-24 14:19:28 -0500

    feat: add CRUD methods to ProductService

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IProduct } from '../interfaces/product.interface';
+import { IProductsReponse } from '../interfaces/products.interface';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProductService {}
+export class ProductService {
+  url = '/api/products';
+  constructor(private http: HttpClient) {}
+
+  getAll() {
+    return this.http.get<IProductsReponse>(this.url);
+  }
+
+  create(product: IProduct) {
+    return this.http.post(this.url, product);
+  }
+
+  getById(id: string) {
+    return this.http.get<IProduct>(`${this.url}/${id}`);
+  }
+
+  update(product: IProduct) {
+    return this.http.put(`${this.url}/${product.id}`, product);
+  }
+
+  deleteById(id: string) {
+    return this.http.delete(`${this.url}/${id}`);
+  }
+}
