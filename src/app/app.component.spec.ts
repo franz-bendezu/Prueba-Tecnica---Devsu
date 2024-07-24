ab01fa12cb8baf80c32d898e094e66b43a3c82b3
Franz Bendezu <franzant20@gmail.com>
2024-07-24 00:20:47 -0500

    refactor: set global styling

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, technical-test-devsu');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Banco');
   });
 });
