caa7106df1b2c8462a169459462fd9a7c899718d
Franz Bendezu <franzant20@gmail.com>
2024-07-24 21:39:07 -0500

    feat: add feature on  products view for display product data from the service in a table

--- src/app/views/products/products.component.ts
+++ src/app/views/products/products.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { IProduct } from '../../interfaces/product.interface';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: IProduct[] = [];
+  loading = true;
+  error: Error | null = null;
+  constructor(private productService: ProductService) {}
 
+  loadProducts() {
+    this.loading = true;
+    this.productService.getAll().subscribe({
+      complete: () => {
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = err;
+        this.loading = false;
+      },
+      next: (products) => {
+        this.products = products.data;
+      },
+    });
+  }
+
+  async ngOnInit() {
+    this.loadProducts();
+  }
 }
