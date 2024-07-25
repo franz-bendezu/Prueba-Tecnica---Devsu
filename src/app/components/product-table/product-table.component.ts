f6ae656d2e64961f2c85a105bb86f4686e2ce250
Franz Bendezu <franzant20@gmail.com>
2024-07-24 23:25:26 -0500

    refactor: improve product table component styling and structure

--- src/app/components/product-table/product-table.component.ts
+++ src/app/components/product-table/product-table.component.ts
@@ -1,14 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { IProduct } from '../../interfaces/product.interface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product-table.component.html',
   styleUrl: './product-table.component.css',
 })
 export class ProductTableComponent {
   @Input() products: IProduct[] = [];
+
+  @Input()
+  pageSizes = [5, 10, 25, 50, 100];
+  pageSize = 10;
+
+  changePageSize(size: number): void {
+    this.pageSize = size;
+  }
 }
