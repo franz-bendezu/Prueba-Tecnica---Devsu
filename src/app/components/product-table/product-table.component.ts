34f6d8aec7c7c44eaad09b2583af6e215c2eb50d
Franz Bendezu <franzant20@gmail.com>
2024-07-25 00:35:49 -0500

    feat: add to product table pagination functionality

--- src/app/components/product-table/product-table.component.ts
+++ src/app/components/product-table/product-table.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { IProduct } from '../../interfaces/product.interface';
 import { FormsModule } from '@angular/forms';
 
@@ -10,14 +10,47 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './product-table.component.html',
   styleUrl: './product-table.component.css',
 })
-export class ProductTableComponent {
-  @Input() products: IProduct[] = [];
-
+export class ProductTableComponent implements OnChanges {
+  @Input()
+  products: IProduct[] = [];
   @Input()
   pageSizes = [5, 10, 25, 50, 100];
+  @Input()
   pageSize = 10;
+  @Input()
+  currentPage = 1;
 
-  changePageSize(size: number): void {
-    this.pageSize = size;
+  items: IProduct[] = [];
+  totalPages = 0;
+
+  changePageSize(newPageSize: number): void {
+    this.pageSize = newPageSize;
+    this.currentPage = 1;
+    this.refreshDisplayedProducts();
+    this.calculateTotalPages();
+  }
+
+  changePage(page: number): void {
+    this.currentPage = page;
+    this.refreshDisplayedProducts();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['products'] || changes['pageSize'] || changes['currentPage']) {
+      this.refreshDisplayedProducts();
+    }
+    if (changes['products'] || changes['pageSize']) {
+      this.calculateTotalPages();
+    }
+  }
+
+  calculateTotalPages(): void {
+    this.totalPages = Math.ceil(this.products.length / this.pageSize);
+  }
+
+  refreshDisplayedProducts(): void {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.items = this.products.slice(startIndex, endIndex);
   }
 }
