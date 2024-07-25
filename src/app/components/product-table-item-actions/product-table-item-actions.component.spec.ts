3274481178d212dcaeeb8123f6fa651fac75a4df
Franz Bendezu <franzant20@gmail.com>
2024-07-25 12:49:54 -0500

    test: add spec cases for emit actions

--- src/app/components/product-table-item-actions/product-table-item-actions.component.spec.ts
+++ src/app/components/product-table-item-actions/product-table-item-actions.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProductTableItemActionsComponent } from './product-table-item-actions.component';
 
 describe('ProductTableItemActionsComponent', () => {
@@ -8,9 +7,8 @@ describe('ProductTableItemActionsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductTableItemActionsComponent]
-    })
-    .compileComponents();
+      imports: [ProductTableItemActionsComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductTableItemActionsComponent);
     component = fixture.componentInstance;
@@ -19,5 +17,25 @@ describe('ProductTableItemActionsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit edit event when onEdit is called', () => {
+    spyOn(component.edit, 'emit');
+    component.onEdit();
+    expect(component.edit.emit).toHaveBeenCalled();
+  });
+
+  it('should emit delete event when onDelete is called', () => {
+    spyOn(component.delete, 'emit');
+    component.onDelete();
+    expect(component.delete.emit).toHaveBeenCalled();
+  });
+
+  it('should toggle showMenu when openMenu is called', () => {
+    component.showMenu = false;
+    component.openMenu();
+    expect(component.showMenu).toBe(true);
+    component.openMenu();
+    expect(component.showMenu).toBe(false);
   });
 });
