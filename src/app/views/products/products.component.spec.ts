59e73e083a688c3c06ad90d7f5f1f2570f4021a6
Franz Bendezu <franzant20@gmail.com>
2024-07-24 21:37:42 -0500

    test: setup test and set spec cases for products view

--- src/app/views/products/products.component.spec.ts
+++ src/app/views/products/products.component.spec.ts
@@ -1,23 +1,76 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../services/product.service';
+import { IProduct } from '../../interfaces/product.interface';
 import { ProductsComponent } from './products.component';
+import { of, throwError } from 'rxjs';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideZoneChangeDetection } from '@angular/core';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let productService: ProductService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductsComponent]
-    })
-    .compileComponents();
+      imports: [CommonModule],
+      providers: [
+        provideZoneChangeDetection({
+          eventCoalescing: true,
+        }),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        ProductService,
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    productService = TestBed.inject(ProductService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load products on initialization', () => {
+    const products: IProduct[] = [
+      {
+        id: '1',
+        name: 'Product 1',
+        description: 'Description 1',
+        logo: '',
+        date_release: '',
+        date_revision: '',
+      },
+      {
+        id: '2',
+        name: 'Product 2',
+        description: 'Description 2',
+        logo: '',
+        date_release: '',
+        date_revision: '',
+      },
+    ];
+    spyOn(productService, 'getAll').and.returnValue(of({ data: products }));
+
+    fixture.detectChanges();
+
+    expect(component.products).toEqual(products);
+    expect(component.loading).toBe(false);
+    expect(component.error).toBeNull();
+  });
+
+  it('should handle error when loading products', () => {
+    const error = new Error('Failed to load products');
+    spyOn(productService, 'getAll').and.returnValue(throwError(() => error));
+
+    fixture.detectChanges();
+
+    expect(component.products).toEqual([]);
+    expect(component.loading).toBe(false);
+    expect(component.error).toBe(error);
   });
 });
