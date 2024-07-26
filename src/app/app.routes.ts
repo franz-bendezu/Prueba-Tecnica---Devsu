6ed5625114ce290cca92975f1cae7accebe0dd22
Franz Bendezu <franzant20@gmail.com>
2024-07-25 22:33:36 -0500

    feat: add view for edit product

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './views/products/products.component';
+import { ProductEditComponent } from './views/product-edit/product-edit.component';
+import { PRODUCTS_PATH, PRODUCTS_ID_PATH } from './constants/routes.contants';
 
 export const routes: Routes = [
+  { path: '', redirectTo: PRODUCTS_PATH, pathMatch: 'full' },
   {
-    path: '',
+    path: PRODUCTS_PATH,
     component: ProductsComponent,
     title: 'Productos Financieros',
+  },
+  {
+    path: PRODUCTS_ID_PATH,
+    component: ProductEditComponent,
+    title: 'Editar Producto',
   },
 ];
