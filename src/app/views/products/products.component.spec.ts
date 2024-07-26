059eae69dfd27d7ca0b193ffe88e18d2f7cf463a
Franz Bendezu <franzant20@gmail.com>
2024-07-25 22:21:06 -0500

    feat: add specs for test the service calls of product and actions

--- src/app/views/products/products.component.spec.ts
+++ src/app/views/products/products.component.spec.ts
@@ -1,4 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../../services/product.service';
 import { IProduct } from '../../interfaces/product.interface';
@@ -6,24 +12,24 @@ import { ProductsComponent } from './products.component';
 import { of, throwError } from 'rxjs';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { provideZoneChangeDetection } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { PARAM_NEW, PRODUCTS_PATH } from '../../constants/routes.contants';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
   let productService: ProductService;
+  const routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CommonModule],
       providers: [
-        provideZoneChangeDetection({
-          eventCoalescing: true,
-        }),
         provideHttpClient(),
         provideHttpClientTesting(),
         ProductService,
+        { provide: Router, useValue: routerSpy },
       ],
     }).compileComponents();
 
@@ -99,8 +105,153 @@ describe('ProductsComponent', () => {
     fixture.detectChanges();
 
     // Check if the table component is rendered and the products are passed to it
-    const productTable = fixture.debugElement.query(By.css('app-product-table'));
+    const productTable = fixture.debugElement.query(
+      By.css('app-product-table')
+    );
     expect(productTable).toBeTruthy();
     expect(productTable.componentInstance.products).toEqual(component.products);
+  });
+
+  it('should handle error when loading products', fakeAsync(() => {
+    const error = new Error('Failed to load products');
+    spyOn(productService, 'getAll').and.returnValue(throwError(() => error));
+
+    fixture.detectChanges();
+
+    expect(component.error).toBe(error);
+  }));
+
+  it('should add a product', () => {
+    spyOn(component, 'addProduct').and.callThrough();
+
+    const button =
+      fixture.debugElement.nativeElement.querySelector('.add-button');
+    button.click();
+
+    expect(component.addProduct).toHaveBeenCalled();
+
+    expect(routerSpy.navigate).toHaveBeenCalledWith([PRODUCTS_PATH, PARAM_NEW]);
+  });
+
+  it('should navigate to edit product', () => {
+    const productMock = {
+      id: '1',
+      name: 'Product 1',
+      description: '',
+      logo: '',
+      date_release: '',
+      date_revision: '',
+    };
+    spyOn(component, 'editProduct').and.callThrough();
+
+    fixture.detectChanges();
+
+    const button = fixture.debugElement.query(By.css('app-product-table'));
+    button.triggerEventHandler('edit', productMock);
+
+    expect(component.editProduct).toHaveBeenCalledWith(productMock);
+
+    expect(routerSpy.navigate).toHaveBeenCalledWith([
+      'products',
+      productMock.id,
+    ]);
+  });
+
+  it('should delete a product', () => {
+    const productMock: IProduct = {
+      id: '1',
+      name: 'Product 1',
+      description: '',
+      logo: '',
+      date_release: '',
+      date_revision: '',
+    };
+    spyOn(productService, 'deleteById').and.returnValue(of({}));
+
+    component.confirmDelete(productMock);
+
+    expect(productService.deleteById).toHaveBeenCalledWith(productMock.id);
+  });
+
+  it('should handle error when deleting a product', fakeAsync(() => {
+    const productMock: IProduct = {
+      id: '1',
+      name: 'Product 1',
+      description: '',
+      logo: '',
+      date_release: '',
+      date_revision: '',
+    };
+    const mockError = new Error('Failed to delete product');
+    spyOn(productService, 'deleteById').and.returnValue(
+      throwError(() => mockError)
+    );
+
+    fixture.detectChanges();
+    
+    component.confirmDelete(productMock);
+
+    expect(component.saveError).toBe(mockError);
+
+  }));
+
+  it('should delete a product', () => {
+    const productMock = {
+      id: '1',
+      name: 'Product 1',
+      description: '',
+      logo: '',
+      date_release: '',
+      date_revision: '',
+    };
+    component.productToDelete = productMock;
+
+    component.deleteProduct(productMock);
+
+    expect(component.openDialog).toBeTrue();
+    expect(component.productToDelete).toEqual(productMock);
+  });
+
+  it('should cancel delete', () => {
+    component.productToDelete = {
+      id: '1',
+      name: 'Product 1',
+      description: '',
+      logo: '',
+      date_release: '',
+      date_revision: '',
+    };
+
+    component.cancelDelete();
+
+    expect(component.openDialog).toBeFalse();
+  });
+
+  it('should edit a product', () => {
+    const productMock = {
+      id: '1',
+      name: 'Product 1',
+      description: '',
+      logo: '',
+      date_release: '',
+      date_revision: '',
+    };
+    spyOn(component, 'editProduct');
+
+    component.editProduct(productMock);
+
+    expect(component.editProduct).toHaveBeenCalledWith(productMock);
+  });
+
+  it('should retry loading products', () => {
+    spyOn(component, 'loadProducts');
+
+    component.error = new Error('Failed to load products');
+    fixture.detectChanges();
+
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+
+    expect(component.loadProducts).toHaveBeenCalled();
   });
 });
