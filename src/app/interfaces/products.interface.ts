7f13b6f8cd469c70264ac3b459d63d6b011dcf03
Franz Bendezu <franzant20@gmail.com>
2024-07-25 23:15:14 -0500

    refactor: add product response for each case

--- src/app/interfaces/products.interface.ts
+++ src/app/interfaces/products.interface.ts
@@ -1,6 +1,19 @@
 import { IProduct } from "./product.interface";
 
-export interface  IProductsReponse {
+export interface  IProductsResponse {
     data: IProduct[];
 }
-    
+
+export interface IBaseResponse {
+    message: string;
+}
+
+export interface IProductSaveResponse extends IBaseResponse {
+    data: IProduct;
+}
+
+export interface IProductCreateResponse extends IProductSaveResponse {}
+
+export interface IProductUpdateResponse extends IProductSaveResponse {}
+
+export interface IProductDeleteResponse extends IBaseResponse {}
