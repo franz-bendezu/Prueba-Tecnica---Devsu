d0a3cbdf86a130a35b53b9ea4b426b3f0d369eda
Franz Bendezu <franzant20@gmail.com>
2024-07-25 22:24:08 -0500

    feat: add validation constants for input fields of product edit

---
+++ src/app/constants/validation.constant.ts
@@ -0,0 +1,8 @@
+export const TEXT_MIN_LENGTH = 1;
+export const ID_MIN_LENGTH = 3;
+export const ID_MAX_LENGTH = 10;
+export const NAME_MIN_LENGTH = 5;
+export const NAME_MAX_LENGTH = 100;
+export const DESCRIPTION_MIN_LENGTH = 10;
+export const DESCRIPTION_MAX_LENGTH = 200;
+export const DATE_MIN_LENGTH = 8;
