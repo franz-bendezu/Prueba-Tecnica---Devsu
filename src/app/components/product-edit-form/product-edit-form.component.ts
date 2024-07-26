1d148807ba16636ccbeb896640ed4e060a247717
Franz Bendezu <franzant20@gmail.com>
2024-07-25 22:34:23 -0500

    feat: add component for the form of product edit

---
+++ src/app/components/product-edit-form/product-edit-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-edit-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-edit-form.component.html',
+  styleUrl: './product-edit-form.component.css'
+})
+export class ProductEditFormComponent {
+
+}
