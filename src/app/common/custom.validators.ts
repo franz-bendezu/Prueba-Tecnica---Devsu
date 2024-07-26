a8454e113960c510386539bdbe4d3bf9145c2788
Franz Bendezu <franzant20@gmail.com>
2024-07-25 22:24:36 -0500

    feat: add custom validators for form input fields

---
+++ src/app/common/custom.validators.ts
@@ -0,0 +1,55 @@
+import {
+  AbstractControl,
+  ValidatorFn,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+
+export function required(message?: string): ValidatorFn {
+  const originalValidator = Validators.required;
+  return (control: AbstractControl): ValidationErrors | null => {
+    const validationResult = originalValidator(control);
+    if (validationResult && validationResult['required']) {
+      return {
+        required: {
+          message: message || 'Este campo requerido',
+        },
+      };
+    }
+    return null;
+  };
+}
+
+export function minLength(minLength: number, message?: string): ValidatorFn {
+  const originalValidator = Validators.minLength(minLength);
+  return (control: AbstractControl): ValidationErrors | null => {
+    const validationResult = originalValidator(control);
+    if (validationResult && validationResult['minlength']) {
+      return {
+        minLength: {
+          requiredLength: validationResult['minlength'].requiredLength,
+          actualLength: validationResult['minlength'].actualLength,
+          message: message || `La longitud mínima es ${minLength}`,
+        },
+      };
+    }
+    return null;
+  };
+}
+
+export function maxLength(maxLength: number, message?: string): ValidatorFn {
+  const originalValidator = Validators.maxLength(maxLength);
+  return (control: AbstractControl): ValidationErrors | null => {
+    const validationResult = originalValidator(control);
+    if (validationResult && validationResult['maxlength']) {
+      return {
+        maxLength: {
+          requiredLength: validationResult['maxlength'].requiredLength,
+          actualLength: validationResult['maxlength'].actualLength,
+          message: message || `La longitud máxima es ${maxLength}`,
+        },
+      };
+    }
+    return null;
+  };
+}
