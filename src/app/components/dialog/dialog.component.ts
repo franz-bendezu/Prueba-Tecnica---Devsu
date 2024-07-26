6ce1f9887264f63672a491020789bc62d2f284c7
Franz Bendezu <franzant20@gmail.com>
2024-07-25 20:04:05 -0500

    feat: setup dialog component styles and add open/close functionality

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-dialog',
   standalone: true,
   imports: [],
   templateUrl: './dialog.component.html',
-  styleUrl: './dialog.component.css'
+  styleUrl: './dialog.component.css',
 })
-export class DialogComponent {
+export class DialogComponent implements OnChanges {
+  @Input()
+  open = false;
+  @Output()
+  openChange = new EventEmitter<boolean>();
 
+  @ViewChild('dialog', { static: true }) dialog!: ElementRef<HTMLDialogElement>;
+
+  private lastOpenState: boolean | null = null;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['open'] && changes['open'].currentValue !== this.lastOpenState) {
+      this.toggleDialog(changes['open'].currentValue);
+      this.lastOpenState = changes['open'].currentValue;
+    }
+  }
+
+  private toggleDialog(isOpen: boolean): void {
+    if (isOpen) {
+      this.dialog.nativeElement.showModal();
+    } else {
+      this.dialog.nativeElement.close();
+    }
+  }
+
+  handleClose() {
+    this.open = false;
+    this.openChange.emit(false);
+  }
 }
