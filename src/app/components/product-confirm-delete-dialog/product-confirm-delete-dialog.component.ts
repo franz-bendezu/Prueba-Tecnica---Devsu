3492fb4afd0a909b233ec296be5c3894a75f75d0
Franz Bendezu <franzant20@gmail.com>
2024-07-25 20:06:14 -0500

    feat: add product-confirm-delete-dialog component

---
+++ src/app/components/product-confirm-delete-dialog/product-confirm-delete-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-confirm-delete-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-confirm-delete-dialog.component.html',
+  styleUrl: './product-confirm-delete-dialog.component.css'
+})
+export class ProductConfirmDeleteDialogComponent {
+
+}
